8a03e0c7956a9e6ff928f9b0113e8e1082a53206
lienowen <lienowen@outlook.com>
2026-07-09 16:09:49 +0800

    Unify product database and ids

--- Assets/Scripts/ProductDatabase.cs
+++ Assets/Scripts/ProductDatabase.cs
@@ -6,24 +6,34 @@ public class ProductData
 {
     public string id;
     public string name;
+    public string category;
     public int buyPrice;
     public int sellPrice;
 }
 
 public class ProductDatabase : MonoBehaviour
 {
+    public static ProductDatabase Instance;
     public List<ProductData> products = new List<ProductData>();
 
     void Awake()
     {
-        products.Add(new ProductData(){id="cola",name="Cola",buyPrice=2,sellPrice=5});
-        products.Add(new ProductData(){id="water",name="Water",buyPrice=1,sellPrice=3});
-        products.Add(new ProductData(){id="milk",name="Milk",buyPrice=3,sellPrice=7});
-        products.Add(new ProductData(){id="chips",name="Chips",buyPrice=2,sellPrice=6});
+        Instance = this;
+
+        if (products.Count == 0)
+            LoadDefaults();
+    }
+
+    void LoadDefaults()
+    {
+        products.Add(new ProductData(){id="cola_box",name="Cola",category="drink",buyPrice=20,sellPrice=35});
+        products.Add(new ProductData(){id="water_box",name="Water",category="drink",buyPrice=15,sellPrice=28});
+        products.Add(new ProductData(){id="milk_box",name="Milk",category="drink",buyPrice=25,sellPrice=42});
+        products.Add(new ProductData(){id="chips_box",name="Chips",category="snack",buyPrice=18,sellPrice=32});
     }
 
     public ProductData GetProduct(string id)
     {
-        return products.Find(x=>x.id==id);
+        return products.Find(x => x.id == id);
     }
 }
