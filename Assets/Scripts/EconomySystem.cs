2171a8e4071347dee3d98bf1fff1aad3fc8eff1f
lienowen <lienowen@outlook.com>
2026-07-09 16:06:49 +0800

    Unify economy API and fix compile references

--- Assets/Scripts/EconomySystem.cs
+++ Assets/Scripts/EconomySystem.cs
@@ -2,24 +2,67 @@ using UnityEngine;
 
 public class EconomySystem : MonoBehaviour
 {
+    public static EconomySystem Instance;
+
+    [Header("Wallet")]
     public int money = 500;
+
+    [Header("Day Totals")]
+    public int totalIncome;
+    public int totalExpense;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    public bool Spend(int amount)
+    {
+        if (amount < 0 || money < amount)
+            return false;
+
+        money -= amount;
+        totalExpense += amount;
+        return true;
+    }
 
     public bool BuyProduct(int price)
     {
-        if(money < price)
-            return false;
+        return Spend(price);
+    }
 
-        money -= price;
-        return true;
+    public void AddIncome(int amount)
+    {
+        if (amount <= 0) return;
+
+        money += amount;
+        totalIncome += amount;
     }
 
     public void SellProduct(int price)
     {
-        money += price;
+        AddIncome(price);
     }
 
     public void AddReward(int amount)
     {
-        money += amount;
+        AddIncome(amount);
+    }
+
+    public int GetProfit()
+    {
+        return totalIncome - totalExpense;
+    }
+
+    public void ResetDayTotals()
+    {
+        totalIncome = 0;
+        totalExpense = 0;
     }
 }
